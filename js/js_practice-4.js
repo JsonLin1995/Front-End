79db139e7efc49e0d881a33aaa10ca6d2e676089
JsonLin1995 <stu900277@gmail.com>
2017-12-01 16:05:42 +0800

    nothing

--- js/js_practice-4.js
+++ js/js_practice-4.js
@@ -4,10 +4,12 @@ $(document).ready(function() {
 					getByName("op2-digits"),
 					getByName("operator"),
 					getByName("other-display"),					
-					getNoNeg()			
+					getFlag("other-noNeg"),					
+					getFlag("other-noEasy")				
 				);
 	var question = { op1: "", op2: "", operator: "", answer: "" };
 	
+	$('#answer').focus();
 	setQuestion( options, question );
 	
 	$( '#save' ).on( 'click', function() {
@@ -16,12 +18,17 @@ $(document).ready(function() {
 					getByName("op2-digits"),
 					getByName("operator"),
 					getByName("other-display"),					
-					getNoNeg()			
+					getFlag("other-noNeg"),					
+					getFlag("other-noEasy")			
 				);
 		setQuestion( options, question );
+		$('#div-play').css({"opacity":"1"});
+		$('#answer').focus();
+		$("#save").attr("disabled", true);
 	} );
 	
 	$( '#answer' ).keypress( function(e) {
+		// check the answer when Enter is pressed
 		if ( e.which == 13 ) {
 			if ( $('#answer').val() == question.answer ) {
 				$('#divQuestion').css( {"color": "white"} );
@@ -52,14 +59,20 @@ $(document).ready(function() {
 	$('#answer').on('focus', function() {
 		this.select();
 	});
+	
+	$('#form-setting input').click( function(){
+		$('#div-play').css({"opacity":"0"});
+		$("#save").attr("disabled", false);
+	} );
 });
 
-function globalOptions( op1Digits, op2Digits, operator, display, noNeg ) {
+function globalOptions( op1Digits, op2Digits, operator, display, noNeg, noEasy ) {
 	this.op1Digits = op1Digits;
 	this.op2Digits = op2Digits;
 	this.operator = operator;
 	this.display = display;
 	this.noNeg = noNeg;
+	this.noEasy = noEasy;
 }
 
 function getByName( checkboxName ) {
@@ -84,8 +97,8 @@ function getByName( checkboxName ) {
 	return checkedOptions;
 }
 
-function getNoNeg() {
-	if ( document.getElementById("other-noNeg").checked ) {
+function getFlag( checkboxName ) {
+	if ( document.getElementById(checkboxName).checked ) {
 		return true;
 	}
 	return false;
@@ -104,19 +117,74 @@ function randomNumber( digits ) {
 	return Math.floor( Math.random() * ( max - min + 1 ) + min );
 }
 
+function randomInRange( min, max ) {
+	//return a number between min & max, both included
+	return Math.floor( Math.random() * ( max - min + 1 ) + min );
+}
+
 function setQuestion( options, question ) {
 	var tmpOp1Digits = randomChoose( options.op1Digits ),
 		tmpOp2Digits = randomChoose( options.op2Digits ),
 		tmpOperator = randomChoose( options.operator ),
 		tmpNoNeg = options.noNeg,
+		tmpNoEasy = options.noEasy,
 		tmpOp1 = randomNumber( tmpOp1Digits ),
 		tmpOp2 = randomNumber( tmpOp2Digits );
+	
+	if ( tmpNoEasy && !(tmpOp1Digits==1 && tmpOp2Digits==1) && tmpOperator == "-" ) {
+		while ( Math.abs( tmpOp1 - tmpOp2 ) <= 10 ){
+			tmpOp1 = randomNumber( tmpOp1Digits ),
+			tmpOp2 = randomNumber( tmpOp2Digits );
+		}
+	}
 	
 	if ( tmpNoNeg && tmpOperator == "-" ) {
 		if ( tmpOp1 < tmpOp2 ){
 			[tmpOp1,tmpOp2] = [tmpOp2,tmpOp1];	//swap
 		}
 	}
+	if ( tmpNoEasy ) {
+		if ( tmpOperator == "+" ) {
+			while ( tmpOp1%10+tmpOp2%10 < 10 ) { 
+				if ( tmpOp1%10 < 9 ) { tmpOp1++; }
+				else { tmpOp2++; }
+			}
+			//avoid too many (tmpOp1%10+tmpOp2%10 == 10)
+			tmpOp1 = randomInRange( tmpOp1, Math.floor(tmpOp1/10)*10 + 9 );
+			tmpOp2 = randomInRange( tmpOp2, Math.floor(tmpOp2/10)*10 + 9 );
+		}
+		else if ( tmpOperator == "-" ) {
+			if ( Math.abs( tmpOp1 - tmpOp2 ) > 10 ) {
+				if ( tmpOp1 > tmpOp2 ) {
+					if ( tmpOp1%10 > tmpOp2%10 ) {
+						// swap the digit in ones
+						var op1Ones = tmpOp1%10,							
+							op2Ones = tmpOp2%10;							
+						tmpOp1 = Math.floor(tmpOp1/10)*10 + op2Ones;
+						tmpOp2 = Math.floor(tmpOp2/10)*10 + op1Ones;
+					}
+					else if ( tmpOp1%10 == tmpOp2%10 ) {
+						if ( tmpOp1%10 > 0 ) { tmpOp1--; }
+						else { tmpOp2++; }
+					}
+				}
+				else {
+					if ( tmpOp1%10 < tmpOp2%10 ) {
+						// swap the digit in ones
+						var op1Ones = tmpOp1%10,							
+							op2Ones = tmpOp2%10;							
+						tmpOp1 = Math.floor(tmpOp1/10)*10 + op2Ones;
+						tmpOp2 = Math.floor(tmpOp2/10)*10 + op1Ones;
+					}
+					else if ( tmpOp1%10 == tmpOp2%10 ) {
+						if ( tmpOp2%10 > 0 ) { tmpOp2--; }
+						else { tmpOp1++; }
+					}
+				}
+			}
+		}
+	}
+	
 	question.op1 = tmpOp1.toString();
 	question.op2 = tmpOp2.toString();
 	question.operator = tmpOperator;
